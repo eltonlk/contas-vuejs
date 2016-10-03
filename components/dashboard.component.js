40cffd64c22cfa4688dc0aef0d434e770ff19385
Elton Luis Kolling <eltonlk@gmail.com>
2016-10-03 17:11:33 -0300

    Alterado para exibir o total de contas a receber e pagar no dashboard, e no total de contas a pagar utilizando ações personalizadas.

--- components/dashboard.component.js
+++ components/dashboard.component.js
@@ -12,17 +12,17 @@ window.dashboardComponent = Vue.extend({
       <tbody>
         <tr>
           <td>Entrada</td>
-          <td>{{ totalReceived | currency }}</td>
+          <td>{{ totalReceives | currency }}</td>
         </tr>
         <tr>
           <td>Saída</td>
-          <td>{{ totalPaid | currency }}</td>
+          <td>{{ totalPays | currency }}</td>
         </tr>
       </thead>
       <tfoot>
         <tr>
           <td>Saldo</td>
-          <td>{{ totalReceived - totalPaid | currency }}</td>
+          <td>{{ totalReceives - totalPays | currency }}</td>
         </tr>
       </tfoot>
     </table>
@@ -33,34 +33,23 @@ window.dashboardComponent = Vue.extend({
   data: function () {
     return {
       title: 'Dashboard',
-      bill_pays: [],
-      bill_receives: this.$root.$children[0].bill_receives
+      bill_receives: this.$root.$children[0].bill_receives,
+      totalPays: 0
     };
   },
   created: function () {
-    this.$http.get('bills').then(function (response) {
-      this.bill_pays = response.data;
+    var self = this;
+
+    BillPay.total().then(function (response) {
+      self.totalPays = response.data.total;
     });
   },
   computed: {
-    totalPaid: function () {
-      var total = 0;
-
-      for(var i in this.bill_pays) {
-        if (this.bill_pays[i].done) {
-          total += this.bill_pays[i].value;
-        }
-      }
-
-      return total;
-    },
-    totalReceived: function () {
+    totalReceives: function () {
       var total = 0;
 
       for(var i in this.bill_receives) {
-        if (this.bill_receives[i].done) {
-          total += this.bill_receives[i].value;
-        }
+        total += this.bill_receives[i].value;
       }
 
       return total;
--- resources.js
+++ resources.js
@@ -1,3 +1,8 @@
 Vue.http.options.root = 'http://127.0.0.1:8000/api';
 
-window.BillPay = Vue.resource('bills{/id}');
+window.BillPay = Vue.resource('bills{/id}', {}, {
+  total: {
+    method: 'GET',
+    url: 'bills/total'
+  }
+});
